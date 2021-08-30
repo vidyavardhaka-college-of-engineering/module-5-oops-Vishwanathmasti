d02e4781968c69afa340d4ec2c4fe8072520a065
Vishwanathmasti <84788762+Vishwanathmasti@users.noreply.github.com>
2021-08-30 13:26:41 +0530

    Done

--- Program-8.cpp
+++ Program-8.cpp
@@ -1,21 +1,16 @@
-//Correct the C++ to find the sum of first n numbers
-
-//Expected input and output
-//Input: 5
-//Output: 15
-
 #include <iostream>
 using namespace std;
 int main()
 {
-    int n, sum = 0, ; //complete the declaration part
+    int n, sum = 0,i=1 ; //complete the declaration part
 
     std::cout << "Enter a positive integer: ";
     std::cin >> n;
 
     while(i<=n)
     {
-        //complete the missing code
+        sum=sum+i;
+        i++; //complete the missing code
         //complete the missing code
     }
 
